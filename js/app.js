2231fad457d8f007e5b20f7d90d5e1fbc58c34a9
Todu66 <giga.todua06@gmail.com>
2023-02-23 22:37:04 +0400

    adding color changes on button

--- js/app.js
+++ js/app.js
@@ -1,59 +1,22 @@
-const cells = document.querySelectorAll('.cell');
-let currentPlayer = 'X';
+// index page first
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
 
-cells.forEach(cell => {
-	cell.addEventListener('click', handleClick, { once: true });
+// Add click event listeners to the buttons
+button1.addEventListener("click", function () {
+ 
+  button1.style.backgroundColor = "#A8BFC9";
+  button1.style.color = "black"
+  // Remove the background color from button2 (if it is currently active)
+  button2.style.backgroundColor = "";
+  button2.style.color = "white"
 });
 
-function handleClick(e) {
-	const cell = e.target;
-	cell.textContent = currentPlayer;
-	cell.classList.add(currentPlayer);
-
-	if (checkWin(currentPlayer)) {
-		alert(`Player ${currentPlayer} wins!`);
-		resetGame();
-	} else if (checkDraw()) {
-		alert('Draw!');
-		resetGame();
-	} else {
-		currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-	}
-}
-
-function checkWin(player) {
-	const winConditions = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-
-	return winConditions.some(condition => {
-		return condition.every(index => {
-			return cells[index].classList.contains(player);
-		});
-	});
-}
-
-function checkDraw() {
-	return [...cells].every(cell => {
-		return cell.classList.contains('X') || cell.classList.contains('O');
-	});
-}
-
-function resetGame() {
-	cells.forEach(cell => {
-		cell.textContent = '';
-		cell.classList.remove('X');
-		cell.classList.remove('O');
-	});
-	currentPlayer = 'X';
-	cells.forEach(cell => {
-		cell.addEventListener('click', handleClick, { once: true });
-	});
-}
+button2.addEventListener("click", function () {
+  // Set the background color of button2 to white
+  button2.style.backgroundColor = "#A8BFC9";
+  button2.style.color = "black"
+  // Remove the background color from button1 (if it is currently active)
+  button1.style.backgroundColor = "";
+  button1.style.color = "white"
+});
