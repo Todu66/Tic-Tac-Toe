547594b713819a2c3bc2bf1bae80f0bf8518527e
Todu66 <giga.todua06@gmail.com>
2023-03-01 17:37:27 +0400

    applying logic

--- js/app.js
+++ js/app.js
@@ -1,22 +1,103 @@
-// index page first
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
+const cells = document.querySelectorAll('.cell');
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+let turn = 'X';
+let gameOver = false;
+let xScore = 0;
+let oScore = 0;
+const xScoreElem = document.getElementById('x-score');
+const oScoreElem = document.getElementById('o-score');
+const resetButton = document.getElementById('reset-button');
+const newGameButton = document.getElementById('new-game-button');
 
-// Add click event listeners to the buttons
-button1.addEventListener("click", function () {
- 
-  button1.style.backgroundColor = "#A8BFC9";
-  button1.style.color = "black"
-  // Remove the background color from button2 (if it is currently active)
-  button2.style.backgroundColor = "";
-  button2.style.color = "white"
-});
+function checkWin() {
+  for (let i = 0; i < winningCombos.length; i++) {
+    const combo = winningCombos[i];
+    const a = cells[combo[0]].textContent;
+    const b = cells[combo[1]].textContent;
+    const c = cells[combo[2]].textContent;
+    if (a === '' || b === '' || c === '') {
+      continue;
+    }
+    if (a === b && b === c) {
+      return [a, combo];
+    }
+  }
+  return null;
+}
 
-button2.addEventListener("click", function () {
-  // Set the background color of button2 to white
-  button2.style.backgroundColor = "#A8BFC9";
-  button2.style.color = "black"
-  // Remove the background color from button1 (if it is currently active)
-  button1.style.backgroundColor = "";
-  button1.style.color = "white"
-});
+function endGame(winner) {
+  if (winner) {
+    for (let i = 0; i < winner[1].length; i++) {
+      cells[winner[1][i]].classList.add('win');
+    }
+    gameOver = true;
+    if (winner[0] === 'X') {
+      xScore++;
+      xScoreElem.textContent = xScore;
+    } else {
+      oScore++;
+      oScoreElem.textContent = oScore;
+    }
+    alert(`${winner[0]} is the winner!`);
+  } else if (!winner && !gameOver) {
+    alert('Tie game!');
+    gameOver = true;
+  }
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  if (cell.textContent !== '' || gameOver) {
+    return;
+  }
+  cell.textContent = turn;
+  cell.classList.add(turn);
+  const winner = checkWin();
+  if (winner || isTie()) {
+    endGame(winner);
+  } else {
+    turn = turn === 'X' ? 'O' : 'X';
+  }
+}
+
+function isTie() {
+  let tie = true;
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i].textContent === '') {
+      tie = false;
+      break;
+    }
+  }
+  return tie;
+}
+
+function resetScores() {
+  xScore = 0;
+  oScore = 0;
+  xScoreElem.textContent = xScore;
+  oScoreElem.textContent = oScore;
+}
+
+function newGame() {
+  gameOver = false;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].classList.remove('win', 'X', 'O');
+    cells[i].textContent = '';
+  }
+  turn = 'X';
+}
+
+resetButton.addEventListener('click', resetScores);
+newGameButton.addEventListener('click', newGame);
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', handleClick);
+}
